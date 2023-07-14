33f86f19ed44d88cf1a2489ea0c0f86adb9aaeda
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-07-14 14:20:46 -0300

    Requisito 11: Implementa casos de teste para a rota GET /login/role

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -3,12 +3,12 @@ import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 import SequelizeUser from '../database/models/SequelizeUser'
-import * as bcrypt from 'bcryptjs'
-import * as jwt from 'jsonwebtoken'
+import * as bcrypt from 'bcryptjs';
 import { app } from '../app';
 import { Response } from 'superagent';
 import { login, loginWithInvalidEmail, loginWithInvalidPassword, loginWithouPassword, loginWithoutEmail, user } from './mocks/users.mocks';
 import JWTUtils from '../utils/JWTUtils';
+import Payload from '../Interfaces/Payload';
 
 chai.use(chaiHttp);
 
@@ -58,5 +58,32 @@ describe('Tests the login route', () => {
     chaiHttpResponse = await chai.request(app).post('/login').send(login);
     expect(chaiHttpResponse).to.have.status(401);
     expect(chaiHttpResponse.body).to.be.have.property('message', 'Invalid email or password');
+  });
+});
+
+describe('Tests the /login/role route', () => {
+  let chaiHttpResponse: Response;
+
+  beforeEach(sinon.restore);
+
+  it('GET /login/role', async () => {
+    const payload: Payload = { role: 'admin', id: 1, username: 'Admin' };
+    sinon.stub(JWTUtils.prototype, 'verify').returns(payload);
+    chaiHttpResponse = await chai.request(app).get('/login/role').set('Authorization', 'valid-token');
+    expect(chaiHttpResponse).to.have.status(200);
+    expect(chaiHttpResponse.body).to.be.have.property('role', 'admin');
+  });
+
+  it('GET /login/role without token', async () => {
+    chaiHttpResponse = await chai.request(app).get('/login/role');
+    expect(chaiHttpResponse).to.have.status(401);
+    expect(chaiHttpResponse.body).to.be.have.property('message', 'Token not found');
+  });
+
+  it('GET /login/role with invalid token', async () => {
+    sinon.stub(JWTUtils.prototype, 'verify').throws();
+    chaiHttpResponse = await chai.request(app).get('/login/role').set('Authorization', 'invalid-token');
+    expect(chaiHttpResponse).to.have.status(401);
+    expect(chaiHttpResponse.body).to.be.have.property('message', 'Token must be a valid token');
   });
 });
