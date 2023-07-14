6f1361d30b1f7f598ac84b1ca35514cb0e4aaa4d
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-07-14 14:52:53 -0300

    Requisito 12: Valida um Bearer token

--- app/backend/src/middlewares/ValidateToken.ts
+++ app/backend/src/middlewares/ValidateToken.ts
@@ -5,15 +5,21 @@ import Payload from '../Interfaces/Payload';
 
 export default class ValidateToken {
   private validator: IAuthentication<Payload>;
+
   constructor(validator: IAuthentication<Payload> = new JWTUtils()) {
     this.validator = validator;
   }
+
+  private static getTokenOnly = (bearerToken: string) => {
+    const splitted = bearerToken.split(' ');
+    return splitted[splitted.length - 1];
+  };
 
   public validate = (req: Request, res: Response, next: NextFunction) => {
     try {
       const token = req.header('authorization');
       if (!token) return res.status(401).json({ message: 'Token not found' });
-      const payload = this.validator.verify(token);
+      const payload = this.validator.verify(ValidateToken.getTokenOnly(token));
       res.locals.user = payload;
       return next();
     } catch (error) {
