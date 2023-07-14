9da122e0f3ab36a936469e93f7be38a42ed1158d
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-07-14 12:40:35 -0300

    Requisito 8: Concluído

--- app/backend/src/middlewares/ValidateLogin.ts
+++ app/backend/src/middlewares/ValidateLogin.ts
@@ -8,17 +8,17 @@ export default class ValidateLogin {
   //   return emailIsFilled && regex.test(email);
   // };
 
-  private static emailWasSent = (email: string): boolean => (
-    email !== null && email !== undefined
-  );
+  // private static emailWasSent = (email: string): boolean => (
+  //   email !== null && email !== undefined
+  // );
 
-  private static passwordWasSent = (password: string) => (
-    password !== null && password !== undefined
-  );
+  // private static passwordWasSent = (password: string) => (
+  //   password !== null && password !== undefined
+  // );
 
   public static validateLogin = (req: Request, res: Response, next: NextFunction) => {
     const { email, password }: LoginData = req.body;
-    if (this.emailWasSent(email) && this.passwordWasSent(password)) { return next(); }
+    if (email && password) { return next(); }
     return res.status(400).json({ message: 'All fields must be filled' });
   };
 }
